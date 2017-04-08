0b0e8491c62226adbbee0f2fe3d92c73edc6c8b9
Artur <kalninsarturs@hotmail.com>
2017-04-08 22:24:51 +0100

    Test

---
+++ TestGitProject/TestGitProject.cpp
@@ -0,0 +1,12 @@
+// TestGitProject.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+//https://itvdn.com/ru/blog/article/using-git-in-visual-studio-enterprise-2015
+
+int main()
+{
+    return 0;
+}
+
